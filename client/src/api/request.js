62d0e63bbb9c06ff3b2431275ce845daf7f21c7f
dengxuan <dengxuandev@163.com>
2026-04-10 12:47:38 +0800

    Modify request.js to add response interceptor

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,27 +1,26 @@
 import axios from "axios";
 
 const service = axios.create({
-    timeout:5000,
+	timeout: 5000,
 });
 
 // 请求拦截器
-service.interceptors.request.use((config)=>{
-    // 拦截请求后，可以做很多处理
-    // 一般是添加token
-    const token = localStorage.getItem("userToken");
-    if(token){
-        config.headers["Authorization"] = "Bearer " + token;
-    }
-    // 请求放行
+service.interceptors.request.use((config) => {
+	// 拦截请求后，可以做很多处理
+	// 一般是添加token
+	const token = localStorage.getItem("userToken");
+	if (token) {
+		config.headers["Authorization"] = "Bearer " + token;
+	}
+	// 请求放行
 
-    return config;
-
+	return config;
 });
 
 // 响应拦截器
-service.interceptors.response.use((response)=>{
-    // 处理响应数据
-    return response.data;
+service.interceptors.response.use((response) => {
+	// 处理响应数据
+	return response.data;
 });
 
 export default service;
