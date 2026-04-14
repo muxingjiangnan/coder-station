e0835f4c8304f7807049d02736f81027ac704386
dengxuan <dengxuandev@163.com>
2026-04-14 15:06:17 +0800

    删除前三名的奖杯图标

--- client/src/components/ScoreRankItem.jsx
+++ client/src/components/ScoreRankItem.jsx
@@ -3,41 +3,7 @@ import { Avatar } from "antd";
 
 function ScoreRankItem(props) {
 	let rankNum = null;
-	switch (props.rank) {
-		case 1: {
-			rankNum = (
-				<div
-					style={{
-						color: "#ffda23",
-						fontSize: "22px",
-					}}
-					className="iconfont icon-jiangbei-"></div>
-			);
-
-			break;
-		}
-		case 2: {
-			rankNum = (
-				<div
-					style={{ color: "#C0C0C0", fontSize: "21px" }}
-					className="iconfont icon-jiangbei-1"></div>
-			);
-			break;
-		}
-		case 3: {
-			rankNum = (
-				<div
-					style={{ color: "#B87333", fontSize: "20px" }}
-					className="iconfont icon-jiangbei-2"></div>
-			);
-			break;
-		}
-
-		default:
-			rankNum = props.rank;
-			break;
-	}
-
+	rankNum = props.rank;
 	return (
 		<div className={styles.container}>
 			{/* 名次  头像和昵称*/}
